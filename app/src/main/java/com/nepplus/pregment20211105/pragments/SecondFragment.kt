b821b524614edb19b8f9c10f08b689c6886c0d0f
이아현 <suk03029@gmail.com>
2021-11-05 11:40:21 +0900

    두번째 프레그먼트 제작 + 배치는 안함

--- app/src/main/java/com/nepplus/pregment20211105/pragments/SecondFragment.kt
+++ app/src/main/java/com/nepplus/pregment20211105/pragments/SecondFragment.kt
@@ -7,25 +7,24 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.nepplus.pregment20211105.R
+import kotlinx.android.synthetic.main.secontfragment.*
 
-class FirstFragment: Fragment() {
-
+class SecondFragment:Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fregment_first,container,false)
+        return inflater.inflate(R.layout.secontfragment, container, false)
 
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
-            super.onActivityCreated(savedInstanceState)
+        super.onActivityCreated(savedInstanceState)
 
-        Toast.makeText(requireContext(), "토스트 입니다", Toast.LENGTH_SHORT).show()
+        btnToast.setOnClickListener {
+            Toast.makeText(requireContext(), "드디어 토스트", Toast.LENGTH_SHORT).show()
+        }
     }
-    
-    }
-
-
+}
